64a4a5bfb2098be95cdb79705ae4b9132e9d93d7
Jaseem-v <muhammedjaseem753@gmail.com>
2021-12-21 13:21:02 +0000

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,3 +167,19 @@ openBtn.addEventListener("click", mobileMenu)
 closeBtn.addEventListener("click", mobileMenuClose)
 overlay.addEventListener("click", mobileMenuClose)
 
+/////////////////////////////////////////////////////
+// scroll
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
